93fc3fae0e532e4b1135e97615e3848a02b51682
roger51 <roger51@yeah.net>
2014-06-01 01:39:03 +0800

    first commit

---
+++ src/main/java/roger/dubbos/Provider.java
@@ -0,0 +1,14 @@
+package roger.dubbos;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;  
+
+public class Provider {  
+   
+    public static void main(String[] args) throws Exception {  
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"/spring/dubbo-demo-provider.xml"});  
+        context.start();  
+   
+        System.in.read(); // 为保证服务一直开着，利用输入流的阻塞来模拟  
+    }  
+   
+}  
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World! server</h2>
+</body>
+</html>
